8f0c3077a455434b4e9c3b2cb9e86e24ce11a25c
Gustavo Rocha da Silva <gtugablue@gmail.com>
2015-04-01 22:34:35 +0100

    Organized code.

--- Indexer/sw.c
+++ Indexer/sw.c
@@ -10,14 +10,45 @@
 
 #define BUF_SIZE 2048
 
+int find_word(const char *word, const char *file);
+int grep(const char *word, const char *file, char *buf);
+
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
+	if (argc != 2)
 	{
-		printf("Usage: sw WORD FILE\n");
+		printf("Usage: sw FILE\n");
 		return 1;
 	}
 
+	if (find_word("Holmes", argv[1])) return 1;
+
+	return 0;
+}
+
+int find_word(const char *word, const char *file)
+{
+	char buf[BUF_SIZE];
+	grep(word, file, buf);
+
+	char *str;
+	if ((str = strtok(buf, "\n")) == NULL) return 1;
+
+	printf("%s: ", word);
+
+	while (str != NULL)
+	{
+		if ((str = strtok(NULL, ":")) == NULL) return 1;
+		unsigned line_number = atoi(str);
+		if ((str = strtok(NULL, "\n")) == NULL) return 1;
+		printf("%d ", line_number);
+	}
+
+	return 0;
+}
+
+int grep(const char *word, const char *file, char *buf)
+{
 	int fd_pipe[2];
 	if (pipe(fd_pipe) < 0) return 1;
 
@@ -31,7 +62,7 @@ int main(int argc, char *argv[])
 	{
 		if (close(fd_pipe[READ]) == -1) return 1;
 		if (dup2(fd_pipe[WRITE], STDOUT_FILENO) == -1) return 1;
-		if (execl("/bin/grep", "grep", "-no", argv[1], argv[2], NULL) == -1) return 1;
+		if (execl("/bin/grep", "grep", "-no", word, file, NULL) == -1) return 1;
 		break; // <-- most useless break ever, but causes a compilation warning if removed
 	}
 	default: // parent
@@ -46,23 +77,8 @@ int main(int argc, char *argv[])
 	if (wait(&status) == -1) return 1;
 	if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) return 1;
 
-	char buf[BUF_SIZE];
-
 	int result;
 	if ((result = read(fd_pipe[READ], buf, BUF_SIZE - 1)) == -1) return 1;
-
-	char *str;
-	if ((str = strtok(buf, "\n")) == NULL) return 1;
-
-	printf("%s: ", argv[1]);
-
-	while (str != NULL)
-	{
-		if ((str = strtok(NULL, ":")) == NULL) return 1;
-		unsigned line_number = atoi(str);
-		if ((str = strtok(NULL, "\n")) == NULL) return 1;
-		printf("%d ", line_number);
-	}
 
 	if (close(fd_pipe[READ]) == -1) return 1;
 
