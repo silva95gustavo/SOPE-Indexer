424eec262beb7aafcc7b8b209bff116b416dbfb8
André Lago <asl_mbc_13@hotmail.com>
2015-04-19 11:55:04 +0100

    Forgot index.h

---
+++ Indexer/index.h
@@ -0,0 +1,10 @@
+#ifndef INDEX_H
+#define INDEX_H
+
+int create_temp_folder();
+int delete_temp_folder(const char *curr_dir);
+int iterate_dir_files(const char *curr_dir, const char *folder, pid_t **pids, int *num_pids);
+int sw(const char *curr_dir, const char *file, pid_t *pid);
+int csc(const char *curr_dir);
+
+#endif
